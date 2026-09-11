0888eef86fd6999c5101f52405dd27218f09087c
Michael Oltarzewski <33165132+ozewski@users.noreply.github.com>
2026-09-11 17:53:40 -0400

    Add Redirection and RedirectOp types

--- shell/command.py
+++ shell/command.py
@@ -1,7 +1,19 @@
 from dataclasses import dataclass, field
+from enum import Enum
+
+class RedirectOp(Enum):
+    READ = "<"
+    # TODO: implement other operations
+
+@dataclass
+class Redirection:
+    # for I/O redirections
+    fd: int         # could be stdin/stdout/stderr or a custom fd
+    op: RedirectOp  # based on syntax of operator
+    target: str
 
 @dataclass
 class Command:
     program: str
     args: list[str] = field(default_factory=list)
-    
+    redirections: list[Redirection] = field(default_factory=list)
